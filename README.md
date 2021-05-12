20e8a3baca64c1a48cb41f9e89eba306c6d3ed38
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-05-12 11:21:02 -0500

    first commit, file setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Moonbelly Makes
---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: "Moonbelly Makes",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "ubyb73h6",
+        dataset: "production",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        // create tracking code here: https://analytics.google.com/analytics/
+        // you need a working URL to link analytics
+        trackingId: "1234",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
