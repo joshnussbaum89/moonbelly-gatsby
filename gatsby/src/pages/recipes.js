16421e27c6b363053a39ca0a5a6c78e3efd822e9
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-05-25 11:57:36 -0500

    style recipe tags and add teaser blog text to photos

--- gatsby/src/pages/recipes.js
+++ gatsby/src/pages/recipes.js
@@ -1,5 +1,3 @@
-// TODO: Add Text to each image
-
 import React from 'react';
 import styled from 'styled-components';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
@@ -7,69 +5,103 @@ import { graphql } from 'gatsby';
 
 const RecipeContainer = styled.div`
   display: grid;
-  grid-template-rows: 1fr 1fr;
+  grid-template-rows: 3fr 1fr;
   gap: 4rem;
 
-  .recipes,
-  .diys {
-    margin: 2rem 0 4rem;
-  }
   .recipes h2,
-  .diys h2 {
+  .tags h2 {
     margin: 2rem 0;
   }
-  .recipes h3,
-  .diys h3 {
-    margin: 1rem 0;
+  .recipes h3 {
+    margin: 2rem 0 1rem;
   }
   .recipe-img {
     width: 100%;
     margin: auto;
     display: block;
   }
+  .tag-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .tag-names {
+    cursor: pointer;
+    text-align: center;
+    border: 1px solid var(--darkPurple);
+    border-radius: 5px;
+    padding: 0.3rem;
+    margin: 0.2rem;
+    transition: 0.2s;
+  }
+  .tag-names:hover {
+    background: var(--darkPurple);
+    color: var(--cream);
+  }
 
   @media (min-width: 768px) {
     margin: 2rem 1rem;
     grid-template-rows: 1fr;
     grid-template-columns: 2fr 1fr;
+
+    .tags {
+      padding-left: 3rem;
+      border-left: 1px dashed var(--mint);
+    }
+    .recipes,
+    .tags {
+      margin: 2rem 0 8rem;
+    }
   }
 `;
 
-const SingleRecipe = ({ recipe }) => {
+const SingleRecipe = ({ recipe, text }) => {
   const image = getImage(recipe.cover.asset.gatsbyImageData);
-  console.log(recipe);
+  const textTeaser = `${text.substring(0, 40)}... Read More`;
+
   return (
     <>
       <h3>{recipe.title}</h3>
+      <p>{textTeaser}</p>
       <GatsbyImage className='recipe-img' image={image} />
     </>
   );
 };
-const SingleTag = ({ tags, diy, recipes }) => {
-  return <h3>{tags}</h3>;
+const SingleTag = ({ tags }) => {
+  return <p className='tag-names'>{tags}</p>;
 };
 
 const Recipes = ({ data }) => {
+  // recipe info
   const recipes = data.posts.nodes.filter((recipe) => recipe.recipe === true);
-  const tagInfo = data.allSanityTag.nodes;
+  // all recipe tags for corresponding posts
+  const tags = recipes
+    .map((recipe) => recipe.tag)
+    .map((tag) => tag.map((tag) => tag.tag))
+    .flat(1)
+    .map((tag) =>
+      tag.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase())
+    );
+  // remove duplicate tags
+  const uniqueTags = [...new Set(tags)];
 
   return (
     <RecipeContainer>
       <div className='recipes'>
-        <h2>Recipes</h2>
+        <h2>Moonbelly Makes ~ Recipes</h2>
         {recipes.map((recipe) => (
-          <SingleRecipe recipe={recipe} />
-        ))}
-      </div>
-      <div className='diys'>
-        <h2>Recipe Tags</h2>
-        {tagInfo.map((tagInfo) => (
-          <SingleTag
-            tags={tagInfo.tag}
-            diy={tagInfo.diy}
-            recipes={tagInfo.recipes}
+          <SingleRecipe
+            recipe={recipe}
+            text={recipe.text[0].children[0].text}
           />
         ))}
+      </div>
+      <div className='tags'>
+        <h2>Categories</h2>
+        <div className='tag-container'>
+          {uniqueTags.map((tag) => (
+            <SingleTag tags={tag} />
+          ))}
+        </div>
       </div>
     </RecipeContainer>
   );
@@ -88,6 +120,9 @@ export const query = graphql`
           children {
             text
           }
+        }
+        tag {
+          tag
         }
         cover {
           asset {
